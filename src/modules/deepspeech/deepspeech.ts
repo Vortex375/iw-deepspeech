46ff8e8300eb84c0b600fd219b4190d6a8b93603
Benjamin Schmitz <vortex@wolpzone.de>
2020-05-02 21:17:19 +0200

    use sox-stream to normalize (amplify) the audio recording; play back the recording for debugging purposes

--- src/modules/deepspeech/deepspeech.ts
+++ src/modules/deepspeech/deepspeech.ts
@@ -2,8 +2,12 @@ import { Service, State } from 'iw-base/lib/registry';
 import { IwDeepstreamClient } from 'iw-base/modules/deepstream-client';
 import { getLogger } from 'iw-base/lib/logging';
 import Ds from 'deepspeech';
+import Sox from 'sox-stream';
 import PulseAudio from 'pulseaudio2';
 import MemoryStream from 'memory-stream';
+// Speaker typedefs appear to be broken
+// tslint:disable-next-line: no-var-requires
+const Speaker = require('speaker');
 
 const log = getLogger('DeepSpeechService');
 
@@ -19,7 +23,7 @@ export class DeepSpeechService extends Service {
   private model: Ds.Model;
   private paContext = new PulseAudio();
 
-  private stream: any;
+  private recordStream: any;
   private memoryBuffer: MemoryStream;
   private recording: boolean;
 
@@ -44,11 +48,11 @@ export class DeepSpeechService extends Service {
       this.paContext.on('connection', () => {
         this.setState(State.OK, 'Ready for voice input');
         resolve();
-      })
+      });
       this.paContext.on('error', (err) => {
         log.error({ err }, 'PulseAudio failed');
         this.setState(State.ERROR, 'PulseAudio failed');
-      })
+      });
     })
     .then(() => {
       this.ds.subscribeEvent('voice-input/request-start-record', () => this.startRecording());
@@ -69,13 +73,37 @@ export class DeepSpeechService extends Service {
       return;
     }
     this.recording = true;
-    this.memoryBuffer = new MemoryStream();
-    this.stream = this.paContext.createRecordStream({
+    this.recordStream = this.paContext.createRecordStream({
       channels: 1,
-      format: 'S16LE',
-      rate: 16000
+      format: 'S24LE',
+      rate: 48000
     });
-    this.stream.pipe(this.memoryBuffer);
+    const transform = Sox({
+      input: {
+        type: 's24',
+        rate: 48000,
+        endian: 'little',
+        channels: 1
+      },
+      output: {
+        type: 's16',
+        rate: 16000,
+        endian: 'little',
+        channels: 1
+      },
+      effects: [
+        // ['noisered'], ['/home/vortex/workspace/audio/speech.noise-profile'], ['0.3'],
+        ['gain'], ['-n'], ['-1']
+      ]
+    });
+    transform.on('error', (err) => {
+      /* sox likes to print stuff to stderr which is treated as 'error' */
+      log.warn('sox', err);
+    });
+    this.memoryBuffer = new MemoryStream();
+    this.recordStream.pipe(transform);
+    transform.pipe(this.memoryBuffer);
+
     this.setState(State.BUSY, 'Recording is active ...');
     this.ds.emitEvent('voice-input/start-record');
 
@@ -92,8 +120,18 @@ export class DeepSpeechService extends Service {
       this.recordingTimer = undefined;
     }
 
-    this.stream.end();
+    this.recordStream.end();
     this.memoryBuffer.on('finish', () => {
+      log.debug('playing back audio');
+      const buffer = this.memoryBuffer.toBuffer();
+      const speaker = new Speaker({
+        channels: 1,
+        bitDepth: 16,
+        signed: true,
+        sampleRate: 16000
+      });
+      speaker.end(buffer);
+      log.debug('playback ok');
       const text = this.runInference(this.memoryBuffer.toBuffer());
       this.ds.emitEvent('voice-input/text', text);
     });
